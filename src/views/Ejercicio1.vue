f8b52e10bbed38299813bff68b388520d197164d
Felipe <sfortizrodriguez@gmail.com>
2026-07-20 17:29:30 -0400

    avances ejercicio 1

--- src/views/Ejercicio1.vue
+++ src/views/Ejercicio1.vue
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
-
+// https://pokeapi.co/api/v2/pokemon/{id o nombre} Endpoint de la PokeAPI, hacer que se pueda buscar cualquier pokemon y mostrar todos sus detalles junto con su foto y nombre.
 
 const mensaje = ref(null);
 
@@ -18,15 +18,40 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="backend">
-        <h1 v-if="mensaje">{{ mensaje.msg }}</h1>
-        <h1 v-else>No se ha podido establecer conexión</h1>
+    <div class="container-principal">    
+        <div class="bloque-izquierdo">    
+            <div class="contenido">
+                <p class="mensaje-conexion" v-if="mensaje">{{ mensaje.msg }}</p>
+                <p class="mensaje-conexion" v-else>No se ha podido establecer conexión</p>
+            </div>
+        </div>
+        <div class="bloque-pokemon">
+            <div class="contenido">
+                <p>Aqui van a ir los pokemons</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 
-.backend h1 {
+.container-principal {
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+}
+
+.contenido {
+    border: 1px solid black;
+    padding: 100px;
+    margin: 10px;
+    text-fit: grow;
+    width: 100vh;
+}
+
+.mensaje-conexion {
     font-family: Google Sans;
 }
 
