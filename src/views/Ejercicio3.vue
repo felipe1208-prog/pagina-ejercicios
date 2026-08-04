2541d3ae1345b8ca9dd110d38b6196c5d111ee27
Felipe <sfortizrodriguez@gmail.com>
2026-08-03 21:17:08 -0400

    empezando vista 3

--- src/views/Ejercicio3.vue
+++ src/views/Ejercicio3.vue
@@ -23,10 +23,15 @@ const calcularSueldo = async () => {
 <template>
     <div class="container">
         <div class="label-boton">
-            <input type="text" class="ingreso">
-            <button class="boton"></button>
+            <h1 class="titulo">Formulario Para Sueldo</h1>
+            <div class="input-y-eso">
+                <input type="text" class="ingreso">
+                <button class="boton"></button>
+            </div>
+            <div class="resultados">
+                Hola
+            </div>
         </div>
-            <div class="resultados"></div>
     </div>
 </template>
 
@@ -35,8 +40,35 @@ const calcularSueldo = async () => {
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
+    min-height: 100vh;
 }
 
+.label-boton {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    width: 700px;
+    height: 600px;
+    flex-direction: column;
+    gap: 50px;
+}
+
+.boton {
+    width: 100px;
+    height: 50px;
+}
+
+.ingreso {
+    
+}
+
+.resultados {
+    display: flex;
+    justify-content: center;
+    border: 1px solid black;
+    height: 200px;
+    width: 500px;
+}
 
 </style>
