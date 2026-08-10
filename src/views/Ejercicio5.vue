d39bcf70c139ed8d8606fdc20a1c3e4bec57fdaa
Felipe <sfortizrodriguez@gmail.com>
2026-08-10 16:18:05 -0400

    funcion post

--- src/views/Ejercicio5.vue
+++ src/views/Ejercicio5.vue
@@ -1,4 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const mensaje = ref("");
+const nombreUsuario = ref("");
+const clave = ref("");
+const email = ref("");
+
+const inicioSesion = async () => {
+    try {
+        const response = await axios.post("https://localhost:7242/api/Ejercicio5", {
+            nombre: nombreUsuario.value,
+            correoElectronico: email.value,
+            clave: clave.value
+        });
+        mensaje.value = response.data;
+    } catch (error) {
+        if (error.response.data.errors) {
+            mensaje.value = Object.values(error.response.data.errors)[0][0];
+        } else if (error.response.data.mensaje) {
+            mensaje.value = error.response.data.mensaje;
+        } else {
+            console.error("Error: ", error);
+        }
+    }
+}
+</script>
 
 <template></template>
 
